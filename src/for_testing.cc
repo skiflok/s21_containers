f68624416e377d6c0bc2de25c3f878737f2d5e97
skiflok <anisimov.a.v@inbox.ru>
2022-10-23 21:42:02 +0300

    first commit

---
+++ src/for_testing.cc
@@ -0,0 +1,10 @@
+//
+// Created by Lok on 23.10.2022.
+//
+
+#include "for_testing.h"
+
+namespace s21 {
+
+
+} // s21
---
+++ src/for_testing.h
@@ -0,0 +1,16 @@
+//
+// Created by Lok on 23.10.2022.
+//
+
+#ifndef S21_MATRIX_SRC_FOR_TESTING_H_
+#define S21_MATRIX_SRC_FOR_TESTING_H_
+
+namespace s21 {
+
+class For_testing {
+
+};
+
+} // s21
+
+#endif //S21_MATRIX_SRC_FOR_TESTING_H_
