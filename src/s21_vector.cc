f6ee765792304a2514a82328b39ed79b06c57938
skiflok <anisimov.a.v@inbox.ru>
2022-10-27 09:54:48 +0300

    vector add

---
+++ src/s21_vector.cc
@@ -0,0 +1,8 @@
+//
+// Created by Lok on 27.10.2022.
+//
+
+#include "s21_vector.h"
+
+namespace s21 {
+} // s21
---
+++ src/s21_vector.h
@@ -0,0 +1,16 @@
+//
+// Created by Lok on 27.10.2022.
+//
+
+#ifndef S21_CONTAINERS_SRC_S21_VECTOR_H_
+#define S21_CONTAINERS_SRC_S21_VECTOR_H_
+
+namespace s21 {
+
+class s21_vector {
+
+};
+
+} // s21
+
+#endif //S21_CONTAINERS_SRC_S21_VECTOR_H_
