d7e50cab3ae58df43413fae64ded13e99f3effc7
Aleksandr <77924467+skiflok@users.noreply.github.com>
2022-10-31 11:07:13 +0300

    sequence

---
+++ src/sequence_container.cc
@@ -0,0 +1,8 @@
+//
+// Created by Lok on 30.10.2022.
+//
+
+#include "sequence_container.h"
+
+namespace s21 {
+} // s21
---
+++ src/sequence_container.h
@@ -0,0 +1,26 @@
+//
+// Created by Lok on 30.10.2022.
+//
+
+#ifndef S21_CONTAINERS_SRC_SEQUENCE_CONTAINER_H_
+#define S21_CONTAINERS_SRC_SEQUENCE_CONTAINER_H_
+
+#include "container.h"
+
+namespace s21 {
+
+class SequenceContainer {
+
+ public:
+  SequenceContainer(){}
+  virtual ~SequenceContainer() =0;
+
+ private:
+
+};
+
+SequenceContainer::~SequenceContainer() {}
+
+} // s21
+
+#endif //S21_CONTAINERS_SRC_SEQUENCE_CONTAINER_H_
